834532bf4fd174058ca13f6f651f2737349cb42b
afilkv <afilkvkv@gmail.com>
2022-10-13 01:25:57 +0530

    Changed 4_cloth.c

--- assignment 1/4_cloth.c
+++ assignment 1/4_cloth.c
@@ -2,18 +2,13 @@
 
 void main(){
 
-    int c1,c2;
+    int answer=1;
     for (int i = 1; i <= 36; i++)
     {
-        c1=36/i;
-        for (int j = 1; j <= 24; j++)
-        {
-            c2=27/j;
-            if(c1==c2){
-                printf("\n Cut each strip of size: %d ",c1);
-                break;
-            }
-        }
-        if(c1==c2)break;
+        if(36%i==0 && 24%i==0){
+            answer=i;
+        }   
     }
+    
+    printf("\n Cut each strip of size: %d ",answer);
 }
